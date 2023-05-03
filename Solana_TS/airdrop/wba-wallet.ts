43542bda5415cf7b41d57b1d315ae1454483e15b
Ceyda <60846011+tomiece317@users.noreply.github.com>
2023-05-03 03:29:02 +0300

    Update wba-wallet.ts

--- Solana_TS/airdrop/wba-wallet.ts
+++ Solana_TS/airdrop/wba-wallet.ts
@@ -2,7 +2,7 @@ import * as anchor from "@project-serum/anchor";
 import * as bs58 from "bs58";
 
 const b = bs58.decode(
-  "27PdmWSo3DGxG8xiSe94G2TxJDEvkyTE7r1TDWsaFQAvsK3VVtpt6vknCUGvZHgQwYTuTd3Z8kkvTz2K84fehqWu"
+  "private-key"
 );
 const j = new Uint8Array(
   b.buffer,
